fdda47d69c598944dde886a542b09206d199f6b8
Zach Bush <zmbush@google.com>
2018-09-10 08:56:16 -0700

    Fix mistype in QWER layout

--- keyboards/ergodox_ez/keymaps/dvorak_zmbush/keymap.c
+++ keyboards/ergodox_ez/keymaps/dvorak_zmbush/keymap.c
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                            KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,           KC_TRNS,
         KC_LALT, KC_TAB,
         KC_PGUP,
-        KC_PGDN, KC_BSPC, KC_END
+        KC_PGDN, KC_ESC, KC_ENT
     ),
 /* Keymap 2: Symbol Layer
  *
