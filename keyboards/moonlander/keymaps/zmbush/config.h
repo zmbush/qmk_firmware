ce92e21ed200a450e587d3887103eb7954403a10
Zachary Bush <zach@zmbush.com>
2021-04-09 11:44:57 -0700

    Add moonlander keymap zmbush

---
+++ keyboards/moonlander/keymaps/zmbush/config.h
@@ -0,0 +1,38 @@
+/* Copyright 2020 ZSA Technology Labs, Inc <@zsa>
+ * Copyright 2020 Jack Humbert <jack.humb@gmail.com>
+ * Copyright 2020 Christopher Courtney <drashna@live.com> (@drashna)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define ORYX_CONFIGURATOR
+#define AUDIO_CLICKY
+#define MOONLANDER_USER_LEDS
+
+#define STARTUP_SONG SONG(    \
+    Q__NOTE(_G5),     \
+    Q__NOTE(_FS5),    \
+    Q__NOTE(_DS5),     \
+    Q__NOTE(_A4),    \
+    Q__NOTE(_GS4),     \
+    Q__NOTE(_E5),     \
+    Q__NOTE(_GS5),     \
+    HD_NOTE(_C6),)
+
+#define DEFAULT_LAYER_SONGS { \
+    SONG(DVORAK_SOUND), \
+    SONG(QWERTY_SOUND), \
+}
